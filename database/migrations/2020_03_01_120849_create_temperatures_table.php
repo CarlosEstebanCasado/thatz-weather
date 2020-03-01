c508a885764926daef1b801b576b031020ef424d
Carlos Esteban <carlosbcn169@gmail.com>
2020-03-01 16:20:03 +0100

    Añadida migracion tabla Temperaturas

---
+++ database/migrations/2020_03_01_120849_create_temperatures_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTemperaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temperatures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('city',50)->unique();
+            $table->smallInteger('cp')->length(4);
+            $table->unsignedSmallInteger('actual_temp');
+            $table->unsignedSmallInteger('day_1_temp')->length(4);
+            $table->unsignedSmallInteger('day_2_temp')->length(4);
+            $table->unsignedSmallInteger('day_3_temp')->length(4);
+            $table->unsignedSmallInteger('day_4_temp')->length(4);
+            $table->unsignedSmallInteger('day_5_temp')->length(4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temperatures');
+    }
+}
